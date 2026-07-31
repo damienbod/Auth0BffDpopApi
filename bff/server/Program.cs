6c42095b7e5a5b67549ce60a81b769bc80c26260
damienbod <damien_bod@hotmail.com>
2026-07-31 19:22:42 +0200

    Switch to Auth0 OpenID Client wrapper

--- bff/server/Program.cs
+++ bff/server/Program.cs
@@ -1,4 +1,5 @@
-﻿using BffAuth0.Server;
+﻿using Auth0.AspNetCore.Authentication;
+using BffAuth0.Server;
 using BffAuth0.Server.Services;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
@@ -6,7 +7,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.JsonWebTokens;
 using Microsoft.IdentityModel.Logging;
 using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
 using NetEscapades.AspNetCore.SecurityHeaders.Infrastructure;
+using System.Security.Cryptography.X509Certificates;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -34,7 +37,7 @@ services.AddSecurityHeaderPolicies()
 services.AddAntiforgery(options =>
 {
     options.HeaderName = "X-XSRF-TOKEN";
-    options.Cookie.Name = "__Host-X-XSRF-TOKEN";
+    options.Cookie.Name = "__Host_Http-X-XSRF-TOKEN";
     options.Cookie.SameSite = SameSiteMode.Strict;
     options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
 });
@@ -42,76 +45,94 @@ services.AddAntiforgery(options =>
 services.AddHttpClient();
 services.AddOptions();
 
-services.AddAuthentication(options =>
+var privatePem = File.ReadAllText(Path.Combine(builder.Environment.ContentRootPath, "rsa256-oidc-private.pem"));
+var publicPem = File.ReadAllText(Path.Combine(builder.Environment.ContentRootPath, "rsa256-oidc-public.pem"));
+
+//var webDpopClientPrivatePem = builder.Configuration.GetValue<string>("WebDpopClientPrivatePem");
+//var webDpopClientPublicPem = builder.Configuration.GetValue<string>("WebDpopClientPublicPem");
+
+var rsaCertificate = X509Certificate2.CreateFromPem(publicPem, privatePem);
+var rsaCertificateKey = new RsaSecurityKey(rsaCertificate.GetRSAPrivateKey());
+
+builder.Services.AddAuth0WebAppAuthentication(options =>
 {
-    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
-})
-.AddCookie(options =>
-{
-    options.Cookie.Name = "__Host-auth0";
-    options.Cookie.SameSite = SameSiteMode.Lax;
-})
-.AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
-{
-    options.Authority = $"https://{configuration["Auth0:Domain"]}";
-    options.ClientId = configuration["Auth0:ClientId"];
-    options.ClientSecret = configuration["Auth0:ClientSecret"];
-    options.ResponseType = OpenIdConnectResponseType.Code;
-    options.Scope.Clear();
-    options.Scope.Add("openid");
-    options.Scope.Add("profile");
-    options.Scope.Add("email");
-    options.Scope.Add("auth0-user-api-one");
-    // options.CallbackPath = new PathString("/signin-oidc");
-    options.ClaimsIssuer = "Auth0";
-    options.SaveTokens = true;
-    options.UsePkce = true;
-    options.GetClaimsFromUserInfoEndpoint = true;
-    options.TokenValidationParameters.NameClaimType = "name";
-
-    options.Events = new OpenIdConnectEvents
-    {
-        OnTokenResponseReceived = context =>
-        {
-            var idToken = context.TokenEndpointResponse.IdToken;
-            return Task.CompletedTask;
-        },
-        // handle the logout redirection 
-        OnRedirectToIdentityProviderForSignOut = (context) =>
-        {
-            var logoutUri = $"https://{configuration["Auth0:Domain"]}/v2/logout?client_id={configuration["Auth0:ClientId"]}";
-
-            var postLogoutUri = context.Properties.RedirectUri;
-            if (!string.IsNullOrEmpty(postLogoutUri))
-            {
-                if (postLogoutUri.StartsWith("/"))
-                {
-                    // transform to absolute
-                    var request = context.Request;
-                    postLogoutUri = request.Scheme + "://" + request.Host + request.PathBase + postLogoutUri;
-                }
-                logoutUri += $"&returnTo={Uri.EscapeDataString(postLogoutUri)}";
-            }
-
-            context.Response.Redirect(logoutUri);
-            context.HandleResponse();
-
-            return Task.CompletedTask;
-        },
-        OnRedirectToIdentityProvider = context =>
-        {
-            // The context's ProtocolMessage can be used to pass along additional query parameters
-            // to Auth0's /authorize endpoint.
-            // 
-            // Set the audience query parameter to the API identifier to ensure the returned Access Tokens can be used
-            // to call protected endpoints on the corresponding API.
-            context.ProtocolMessage.SetParameter("audience", "https://auth0-api1");
-
-            return Task.FromResult(0);
-        }
-    };
+    options.Domain = "dev-damienbod.eu.auth0.com"; // builder.Configuration["Auth0:Domain"];
+    options.ClientId = "0erfhb9bqdefyOZp2x4b8lIP5Ampdf2P"; // builder.Configuration["Auth0:ClientId"];
+    options.Scope = "auth0-user-api-one";
+    options.ClientAssertionSecurityKey = rsaCertificateKey;
+    options.ClientAssertionSecurityKeyAlgorithm = "RS256";
 });
+
+//services.AddAuthentication(options =>
+//{
+//    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+//    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+//})
+//.AddCookie(options =>
+//{
+//    options.Cookie.Name = "__Host_Http-auth0";
+//    options.Cookie.SameSite = SameSiteMode.Lax;
+//})
+//.AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+//{
+//    options.Authority = $"https://{configuration["Auth0:Domain"]}";
+//    options.ClientId = configuration["Auth0:ClientId"];
+//    options.ClientSecret = configuration["Auth0:ClientSecret"];
+//    options.ResponseType = OpenIdConnectResponseType.Code;
+//    options.Scope.Clear();
+//    options.Scope.Add("openid");
+//    options.Scope.Add("profile");
+//    options.Scope.Add("email");
+//    options.Scope.Add("auth0-user-api-one");
+//    // options.CallbackPath = new PathString("/signin-oidc");
+//    options.ClaimsIssuer = "Auth0";
+//    options.SaveTokens = true;
+//    options.UsePkce = true;
+//    options.GetClaimsFromUserInfoEndpoint = true;
+//    options.TokenValidationParameters.NameClaimType = "name";
+
+//    options.Events = new OpenIdConnectEvents
+//    {
+//        OnTokenResponseReceived = context =>
+//        {
+//            var idToken = context.TokenEndpointResponse.IdToken;
+//            return Task.CompletedTask;
+//        },
+//        // handle the logout redirection 
+//        OnRedirectToIdentityProviderForSignOut = (context) =>
+//        {
+//            var logoutUri = $"https://{configuration["Auth0:Domain"]}/v2/logout?client_id={configuration["Auth0:ClientId"]}";
+
+//            var postLogoutUri = context.Properties.RedirectUri;
+//            if (!string.IsNullOrEmpty(postLogoutUri))
+//            {
+//                if (postLogoutUri.StartsWith("/"))
+//                {
+//                    // transform to absolute
+//                    var request = context.Request;
+//                    postLogoutUri = request.Scheme + "://" + request.Host + request.PathBase + postLogoutUri;
+//                }
+//                logoutUri += $"&returnTo={Uri.EscapeDataString(postLogoutUri)}";
+//            }
+
+//            context.Response.Redirect(logoutUri);
+//            context.HandleResponse();
+
+//            return Task.CompletedTask;
+//        },
+//        OnRedirectToIdentityProvider = context =>
+//        {
+//            // The context's ProtocolMessage can be used to pass along additional query parameters
+//            // to Auth0's /authorize endpoint.
+//            // 
+//            // Set the audience query parameter to the API identifier to ensure the returned Access Tokens can be used
+//            // to call protected endpoints on the corresponding API.
+//            context.ProtocolMessage.SetParameter("audience", "https://auth0-api1");
+
+//            return Task.FromResult(0);
+//        }
+//    };
+//});
 
 services.AddControllersWithViews(options =>
     options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute()));
