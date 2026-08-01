407bf5359bd7219b59d98aa521bb8a1358061b1a
damienbod <damien_bod@hotmail.com>
2026-08-01 09:25:33 +0200

    Update API

--- Api/Program.cs
+++ Api/Program.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.OpenApi;
 using Microsoft.IdentityModel.JsonWebTokens;
 using Microsoft.IdentityModel.Logging;
-using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi;
 using NetEscapades.AspNetCore.SecurityHeaders.Infrastructure;
 using WebApi;
