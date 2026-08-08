17a81b87446a67436b40b612c22feff1003bc147
damienbod <damien_bod@hotmail.com>
2026-08-08 15:50:03 +0200

    Add Aspire

--- AppAspireHost/AppHost.cs
+++ AppAspireHost/AppHost.cs
@@ -1,3 +1,22 @@
+using Microsoft.Extensions.Hosting;
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+if (builder.Environment.IsDevelopment())
+{
+    var angularFrontend = builder.AddJavaScriptApp("angular", "../bff/ui", "start")
+        .WithHttpsEndpoint(port: 3000, 4201, env: "BASE_URL");
+
+    builder.AddProject<Projects.BffAuth0_Server>("bffauth0-server")
+        .WithReference(angularFrontend)
+        .WaitFor(angularFrontend)
+        .WithExternalHttpEndpoints();
+}
+else
+{
+    // Hint: to make this work, the deployment pipeline must execute npm run build
+    builder.AddProject<Projects.BffAuth0_Server>("bffauth0-server")
+        .WithExternalHttpEndpoints();
+}
 
 builder.Build().Run();
