f3068f1e3ee344e2f7058baa0760295cf2d1e843
damienbod <damien_bod@hotmail.com>
2026-08-09 08:17:17 +0200

    Setup configuration

--- AppAspireHost/AppHost.cs
+++ AppAspireHost/AppHost.cs
@@ -1,25 +1,55 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Hosting;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.WebApi>("webapi")
-    .WithExternalHttpEndpoints();
+IResourceBuilder<ProjectResource>? webApi = null;
+IResourceBuilder<ProjectResource>? webApplication = null;
+
+// Parameters for the web application
+var webOidcClientPrivatePem = builder.AddParameter("WebOidcClientPrivatePem", secret: true);
+var webOidcClientPublicPem = builder.AddParameter("WebOidcClientPublicPem");
+var webDpopClientPrivatePem = builder.AddParameter("WebDpopClientPrivatePem", secret: true);
+var webDpopClientPublicPem = builder.AddParameter("WebDpopClientPublicPem");
+
+// Parameters for the web API
+var apiAuth0Authority = builder.AddParameter("ApiAuth0Authority");
+var apiAuth0Audience = builder.AddParameter("ApiAuth0Audience");
+var apiAuth0Domain = builder.AddParameter("ApiAuth0Domain");
+var apiDeploySwaggerUI = builder.AddParameter("ApiDeploySwaggerUI");
+
+webApi = builder.AddProject<Projects.WebApi>("webapi")
+    .WithExternalHttpEndpoints()
+    .WithEnvironment("ApiAuth0Authority", apiAuth0Authority)
+    .WithEnvironment("ApiAuth0Audience", apiAuth0Audience)
+    .WithEnvironment("ApiAuth0Domain", apiAuth0Domain)
+    .WithEnvironment("ApiDeploySwaggerUI", apiDeploySwaggerUI);
 
 if (builder.Environment.IsDevelopment())
 {
     var angularFrontend = builder.AddJavaScriptApp("angular", "../bff/ui", "start")
         .WithHttpsEndpoint(port: 3000, 4201, env: "BASE_URL");
 
-    builder.AddProject<Projects.BffAuth0_Server>("bffauth0-server")
+    webApplication =builder.AddProject<Projects.BffAuth0_Server>("bffauth0-server")
         .WithReference(angularFrontend)
+        .WithReference(webApi)
         .WaitFor(angularFrontend)
         .WithExternalHttpEndpoints();
 }
 else
 {
-    // Hint: to make this work, the deployment pipeline must execute npm run build
-    builder.AddProject<Projects.BffAuth0_Server>("bffauth0-server")
-        .WithExternalHttpEndpoints();
+    // Hint: to make this work, the deployment pipeline must execute npm run build 
+    // which deploys to the wwwroot folder of the bffauth0-server project.
+    webApplication = builder.AddProject<Projects.BffAuth0_Server>("bffauth0-server")
+        .WithExternalHttpEndpoints()
+        .WithReference(webApi)
+        .WaitFor(webApi)
+        //.WithEnvironment("WebOidcAuthority", webOidcAuthority)
+        //.WithEnvironment("WebOidcClientId", webOidcClientId)
+        .WithEnvironment("WebOidcClientPrivatePem", webOidcClientPrivatePem)
+        .WithEnvironment("WebOidcClientPublicPem", webOidcClientPublicPem)
+        .WithEnvironment("WebDpopClientPrivatePem", webDpopClientPrivatePem)
+        .WithEnvironment("WebDpopClientPublicPem", webDpopClientPublicPem);
 }
 
 builder.Build().Run();
